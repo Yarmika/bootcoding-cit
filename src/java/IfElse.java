61fb712eead64dc752141acf13ac8efaa8c2827c
Yarmika <492.yarmika@gmail.com>
2023-08-18 14:58:47 +0530

    my first program

---
+++ src/java/IfElse.java
@@ -0,0 +1,13 @@
+package src.java;
+
+public class IfElse {
+    public static void main(String[] arg) {
+        int number;
+        number = 29;
+        if (number%2 != 0) {
+            System.out.print(number + "is even number");
+        } else {
+            System.out.print(number + " is odd number");
+        }
+    }
+}
