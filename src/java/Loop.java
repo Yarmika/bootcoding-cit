2c713e28fecc3fcf3aa47047c69427a9bf7c0ab0
Yarmika <492.yarmika@gmail.com>
2023-08-18 16:15:15 +0530

    my first program

---
+++ src/java/Loop.java
@@ -0,0 +1,10 @@
+package src.java;
+
+public class Loop {
+    public static void main(String[] arg) {
+        int  range = 10 ;
+        for(int i =1 ; i<=range ; i++){
+            System.out.println("   "+i);
+        }
+    }
+}
