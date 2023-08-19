fd8e16cfaf7d2701c4d92c71249de78683e099b0
Yarmika <492.yarmika@gmail.com>
2023-08-19 12:11:49 +0530

    day two of java

---
+++ src/java/Array.java
@@ -0,0 +1,9 @@
+package src.java;
+
+public class Array {
+    public static void main(String[] arg){
+        String name[] = {"sham","ram","sita","gita"};
+        System.out.println(name[3]);
+        System.out.println(name[0]);
+    }
+}
